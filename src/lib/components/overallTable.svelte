397a54cf28fed34a59088500de367b51f77f6995
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-08 11:16:44 +0800

    Update overallTable.svelte

--- src/lib/components/overallTable.svelte
+++ src/lib/components/overallTable.svelte
@@ -77,10 +77,7 @@
             <div class="flex items-center space-x-3">
               <div class="avatar">
                     <div class="mask mask-squircle w-12 h-12">
-                        <img
-                            src={'logo-' + row['__EMPTY'].toLowerCase().replace(/\s+/g, '-') + '.png'}
-                            alt={row['__EMPTY']}
-                        />
+                 
                     </div>
               </div>
               <div>
