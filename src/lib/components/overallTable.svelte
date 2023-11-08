dfce35a8bbde65f15a09741039621a114555ee88
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-08 10:33:30 +0800

    Update overallTable.svelte

--- src/lib/components/overallTable.svelte
+++ src/lib/components/overallTable.svelte
@@ -1,46 +1,24 @@
 
 <script lang="ts">
     export let data = [] as any[];
-    let regionWithScores: { [key: string]: any } = {};
+    let regionWithScores: []
 
 
     data.forEach((game) => {
+        console.log(game)
+        
         if (game['__EMPTY'] === 'HQ') {
-            console.log(game)
+            regionWithScores.push({'__EMPTY_1': })
         }
+
+    
     })
-
-
-    /*
-
-
-    data.forEach((game) => {
-        game.forEach((region) => {
-            if (region['__EMPTY'] === 'HQ') {
-                if (!regionWithScores[region['__EMPTY']]) {
-                    regionWithScores[region['__EMPTY']] = {};
-                }
-                regionWithScores[region['__EMPTY']]['__EMPTY_1'] = region['__EMPTY_1'];
-                regionWithScores[region['__EMPTY']]['__EMPTY_2'] = region['__EMPTY_2'];
-                regionWithScores[region['__EMPTY']]['__EMPTY_3'] = region['__EMPTY_3'];
-                regionWithScores[region['__EMPTY']]['__EMPTY_4'] = region['__EMPTY_4'];
-            }
-        });
-    });
-
-    const regionArray = Object.values(regionWithScores);
-
-    regionArray.sort((a, b) => calculateTotalScore(b) - calculateTotalScore(a));
-
-    console.log(regionArray);
-
 
 
     function calculateTotalScore(row: any) {
         return parseInt(row['__EMPTY_1']) * 4 + parseInt(row['__EMPTY_2']) * 3 + parseInt(row['__EMPTY_3']) * 2 + parseInt(row['__EMPTY_4']) * 1;
     }
 
-    */
 </script>
 
 
