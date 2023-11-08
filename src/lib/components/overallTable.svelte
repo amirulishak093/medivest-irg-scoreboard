58a5aef2bc3129c2dffd16d48cd3e6875b53e7ea
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-08 09:21:44 +0800

    Update overallTable.svelte

--- src/lib/components/overallTable.svelte
+++ src/lib/components/overallTable.svelte
@@ -4,12 +4,12 @@
 
    let regionWithScores = []
 
-/*
     if (data) {
       data.forEach((game) => {
         game.forEach((region) => {
           if (region['__EMPTY'] === 'HQ') {
             regionWithScores.push({
+              region: 'HQ',
               __EMPTY_1: parseInt(region['__EMPTY_1']),
               __EMPTY_2: parseInt(region['__EMPTY_2']),
               __EMPTY_3: parseInt(region['__EMPTY_3']),
@@ -19,15 +19,17 @@
         });
       });
     
-      regionWithScores.sort((a, b) => calculateTotalScore(b) - calculateTotalScore(a));
+      // regionWithScores.sort((a, b) => calculateTotalScore(b) - calculateTotalScore(a));
     
-    //  console.log(regionWithScores);
+   
     }
+
+     console.log(regionWithScores);
 
 
     function calculateTotalScore(row: any) {
       return parseInt(row['__EMPTY_1']) * 4 + parseInt(row['__EMPTY_2']) * 3  + parseInt(row['__EMPTY_3']) * 2  + parseInt(row['__EMPTY_4']) * 1 
-    } */
+    } 
 
 </script>
 
