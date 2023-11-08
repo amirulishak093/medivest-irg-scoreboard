45179d299216ecb9d1f51abd5a11ec33df83bb8c
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-08 11:32:58 +0800

    Update overallTable.svelte

--- src/lib/components/overallTable.svelte
+++ src/lib/components/overallTable.svelte
@@ -31,7 +31,7 @@
       return parseInt(row['__EMPTY_1']) * 4 + parseInt(row['__EMPTY_2']) * 3  + parseInt(row['__EMPTY_3']) * 2  + parseInt(row['__EMPTY_4']) * 1 
     }
 
-    calculateRegionsSums(data);
+    calculateRegionSums(data);
 
 </script>
 
