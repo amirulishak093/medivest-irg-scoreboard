7ab5402987cc99f36e8222f16469432105c5d62d
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-08 09:01:22 +0800

    Update overallTable.svelte

--- src/lib/components/overallTable.svelte
+++ src/lib/components/overallTable.svelte
@@ -2,32 +2,30 @@
 <script lang="ts">
     export let data = [] as any[]
 
-    $: regionWithScores = {}
+    $: regionWithScores = []
 
 
 
-    $: {
+  $: {
+    if (data) {
+      data.forEach((game) => {
+        game.forEach((region) => {
+          if (region['__EMPTY'] === 'HQ') {
+            regionWithScores.push({
+              __EMPTY_1: parseInt(region['__EMPTY_1']),
+              __EMPTY_2: parseInt(region['__EMPTY_2']),
+              __EMPTY_3: parseInt(region['__EMPTY_3']),
+              __EMPTY_4: parseInt(region['__EMPTY_4']),
+            });
+          }
+        });
+      });
 
-        if (data) {
-           data.forEach((game) => {
-              game.forEach((region) => {
-                if (region['__EMPTY'] === 'HQ') {
-                  regionWithScores[region['__EMPTY']]['__EMPTY_1'] = region['__EMPTY_1'];
-                  regionWithScores[region['__EMPTY']]['__EMPTY_2'] = region['__EMPTY_2'];
-                  regionWithScores[region['__EMPTY']]['__EMPTY_3'] = region['__EMPTY_3'];
-                  regionWithScores[region['__EMPTY']]['__EMPTY_4'] = region['__EMPTY_4'];
-      
-                }
-              })
-           })
+      regionWithScores.sort((a, b) => calculateTotalScore(b) - calculateTotalScore(a));
 
-
-          regionWithScores.sort((a, b) => calculateTotalScore(b) - calculateTotalScore(a));
-
-          console.log(regionWithScores);
-        }
-
+      console.log(regionWithScores);
     }
+  }
 
     function calculateTotalScore(row: any) {
       return parseInt(row['__EMPTY_1']) * 4 + parseInt(row['__EMPTY_2']) * 3  + parseInt(row['__EMPTY_3']) * 2  + parseInt(row['__EMPTY_4']) * 1 
