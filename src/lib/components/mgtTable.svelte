b151e469dc668c099b2a84817788291f907296de
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-08 15:30:50 +0800

    Create mgtTable.svelte

---
+++ src/lib/components/mgtTable.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+    export let data = [] as any[]
+
+    data.sort((a, b) => calculateTotalScore(b) - calculateTotalScore(a));
+
+    function calculateTotalScore(row: any) {
+      return parseInt(row['__EMPTY_1']) + parseInt(row['__EMPTY_2']) + parseInt(row['__EMPTY_3']);
+    }
+
+</script>
+
+
+<div class="overflow-x-auto mt-4">
+  <table class="bg-white table">
+ 
+    <thead>
+      <tr>
+        <th />
+        <th>Region</th>
+    <th>Professional Juries (85%)</th>
+    <th>Public Votes (15%)</th>
+    <th>External (5%)</th>
+      <th> Points </th>
+      </tr>
+    </thead>
+
+    <tbody>
+      {#each data as row, index}
+        <tr class={index === 0 ? "active" : ""}>
+          <th>{index + 1}</th>
+          <td>
+            <div class="flex items-center space-x-3">
+              <div class="avatar">
+                    <div class="mask mask-squircle w-12 h-12">
+                        <img
+                            src={'logo-' + row['__EMPTY'].toLowerCase().replace(/\s+/g, '-') + '.png'}
+                            alt={row['__EMPTY']}
+                        />
+                    </div>
+              </div>
+              <div>
+                <div>{row['__EMPTY']}</div>
+              </div>
+            </div>
+          </td>
+          <td class="text-center"> {row['__EMPTY_1']} </td>
+          <td class="text-center"> {row['__EMPTY_2']} </td>
+          <td class="text-center"> {row['__EMPTY_3']} </td>
+          <td class="font-bold"> {calculateTotalScore(row)} </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
+
+
