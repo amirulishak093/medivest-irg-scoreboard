984f59bb4b8c90f2b8dd9ddefa20b18e43a40226
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-08 10:16:01 +0800

    Update overallTable.svelte

--- src/lib/components/overallTable.svelte
+++ src/lib/components/overallTable.svelte
@@ -1,41 +1,35 @@
 
 <script lang="ts">
-    export let data = [] as any[]
+    export let data = [] as any[];
+    let regionWithScores: { [key: string]: any } = {};
 
-   $: regionWithScores = []
+    data.forEach((game) => {
+        game.forEach((region) => {
+            if (region['__EMPTY'] === 'HQ') {
+                if (!regionWithScores[region['__EMPTY']]) {
+                    regionWithScores[region['__EMPTY']] = {};
+                }
+                regionWithScores[region['__EMPTY']]['__EMPTY_1'] = region['__EMPTY_1'];
+                regionWithScores[region['__EMPTY']]['__EMPTY_2'] = region['__EMPTY_2'];
+                regionWithScores[region['__EMPTY']]['__EMPTY_3'] = region['__EMPTY_3'];
+                regionWithScores[region['__EMPTY']]['__EMPTY_4'] = region['__EMPTY_4'];
+            }
+        });
+    });
 
-    $: {
+    const regionArray = Object.values(regionWithScores);
 
-        if (data) {
-          data.forEach((game) => {
+    regionArray.sort((a, b) => calculateTotalScore(b) - calculateTotalScore(a));
 
-            game.forEach((region) => {
-              console.log(region)
-              if (region['__EMPTY'] === 'HQ') {
-                regionWithScores.push({
-                  region: 'HQ',
-                  __EMPTY_1: parseInt(region['__EMPTY_1']),
-                  __EMPTY_2: parseInt(region['__EMPTY_2']),
-                  __EMPTY_3: parseInt(region['__EMPTY_3']),
-                  __EMPTY_4: parseInt(region['__EMPTY_4']),
-                });
-              }
-            });
-          });
-        
-           regionWithScores.sort((a, b) => calculateTotalScore(b) - calculateTotalScore(a));
-       
-        }
-
-    }
+    console.log(regionArray);
 
 
 
     function calculateTotalScore(row: any) {
-      return parseInt(row['__EMPTY_1']) * 4 + parseInt(row['__EMPTY_2']) * 3  + parseInt(row['__EMPTY_3']) * 2  + parseInt(row['__EMPTY_4']) * 1 
-    } 
-
+        return parseInt(row['__EMPTY_1']) * 4 + parseInt(row['__EMPTY_2']) * 3 + parseInt(row['__EMPTY_3']) * 2 + parseInt(row['__EMPTY_4']) * 1;
+    }
 </script>
+
 
 
 
