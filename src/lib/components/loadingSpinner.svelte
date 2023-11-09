aad24f1bc6bd814d9e52c065b615c0016538b7e9
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-09 15:34:05 +0800

    Update loadingSpinner.svelte

--- src/lib/components/loadingSpinner.svelte
+++ src/lib/components/loadingSpinner.svelte
@@ -2,7 +2,6 @@
 	import { fade } from 'svelte/transition';
 </script>
 
-
 <span
 	transition:fade={{ delay: 150, duration: 150 }}
 	class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 loading loading-spinner loading-lg"
