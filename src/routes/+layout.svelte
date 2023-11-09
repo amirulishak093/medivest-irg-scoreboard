72070a49ac4abf3835ebc704fb728b05bc798e8a
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-09 13:50:10 +0800

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,9 @@
   import '@fontsource/poppins';
   import '@fontsource/poppins/500.css';
   import '@fontsource/poppins/700.css';
+	import { fade } from 'svelte/transition';
+	import LoadingSpinner from '$lib/components/loadingSpinner.svelte';
+	import { isLoading } from '$lib/store';
 </script>
 
 <div class="w-full relative min-h-screen">
@@ -11,7 +14,13 @@
   </div>
 
   <div class="w-full max-w-[800px] mx-auto px-4 pb-12">
-    <slot />
+    		{#if $isLoading}
+			<LoadingSpinner />
+		{:else}
+			<div class="pb-8" transition:fade={{ delay: 150, duration: 150 }}>
+				<slot />
+			</div>
+		{/if}
   </div>
 </div>
 
