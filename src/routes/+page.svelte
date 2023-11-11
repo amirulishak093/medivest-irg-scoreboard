b178cd7c65ba346a264ab9b9ef0bfc8a4f4964c8
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-11-11 16:05:56 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -82,7 +82,7 @@
 		{:else if $page === 7}
 		    <Table data={esportData} />
 		{:else if $page === 8}
-		    <OverallTable data={[...tableTennisData,...badmintonData,...netballData,...bowlingData,...carromData,...footballData,...esportData, ...sprintData, ...medivestGotTalentData] />
+		    <OverallTable data={[...tableTennisData,...badmintonData,...netballData,...bowlingData,...carromData,...footballData,...esportData, ...sprintData, ...medivestGotTalentData]} />
 		{:else if $page === 9}
 		    <BestAthleteTable data={menAthletesData} />
 		{:else if $page === 10}
